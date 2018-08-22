42ac727cb1a7fdaa1cfcd92705e29c447271e609
coleman-word <admin606@protonmail.com>
2018-08-22 15:25:54 -0600

    fix main.go

--- main.go
+++ main.go
@@ -23,13 +23,13 @@ func main() {
 	mux.GET("/apply", apply)
 	mux.POST("/apply", applyProcess)
 	mux.GET("/user/:name", user)
-	mux.GET("/blog/category/:article", blogRead)
+	mux.GET("/blog/:category/:article", blogRead)
 	mux.POST("/blog/:category/:article", blogWrite)
 	http.ListenAndServe(":8080", mux)
 }
 
 func user(w http.ResponseWriter, req *http.Request, ps httprouter.Params) {
-	fmt.Fprintf(w, "USER, %s\n", ps.ByName("name"))
+	fmt.Fprintf(w, "USER, %s!\n", ps.ByName("name"))
 }
 
 func blogRead(w http.ResponseWriter, req *http.Request, ps httprouter.Params) {
@@ -42,28 +42,28 @@ func blogWrite(w http.ResponseWriter, req *http.Request, ps httprouter.Params) {
 	fmt.Fprintf(w, "WRITE ARTICLE, %s!\n", ps.ByName("article"))
 }
 
-func index(w http.ResponseWriter, req *http.Request, ps httprouter.Params) {
+func index(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
 	err := tpl.ExecuteTemplate(w, "index.gohtml", nil)
 	HandleError(w, err)
 }
 
-func about(w http.ResponseWriter, req *http.Request, ps httprouter.Params) {
-	err := tpl.ExecuteTemplate(w, "index.gohtml", nil)
+func about(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
+	err := tpl.ExecuteTemplate(w, "about.gohtml", nil)
 	HandleError(w, err)
 }
 
-func contact(w http.ResponseWriter, req *http.Request, ps httprouter.Params) {
-	err := tpl.ExecuteTemplate(w, "index.gohtml", nil)
+func contact(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
+	err := tpl.ExecuteTemplate(w, "contact.gohtml", nil)
 	HandleError(w, err)
 }
 
-func apply(w http.ResponseWriter, req *http.Request, ps httprouter.Params) {
-	err := tpl.ExecuteTemplate(w, "index.gohtml", nil)
+func apply(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
+	err := tpl.ExecuteTemplate(w, "apply.gohtml", nil)
 	HandleError(w, err)
 }
 
-func applyProcess(w http.ResponseWriter, req *http.Request, ps httprouter.Params) {
-	err := tpl.ExecuteTemplate(w, "index.gohtml", nil)
+func applyProcess(w http.ResponseWriter, req *http.Request, _ httprouter.Params) {
+	err := tpl.ExecuteTemplate(w, "applyProcess.gohtml", nil)
 	HandleError(w, err)
 }
 
